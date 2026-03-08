f518fc74457bb03e9c37f16fd2b7bf211dbd40a2
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-08 09:08:51 +0530

    Enhance DashboardHeader: Add user role display and logout functionality

--- client/src/components/dashboard/DashboardHeader.jsx
+++ client/src/components/dashboard/DashboardHeader.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Bell } from "lucide-react";
+import { Bell, LogOut } from "lucide-react";
+import { useAuth } from '../../context/auth';
+import { useState, useEffect} from 'react';
 
 const DashboardHeader = () => {
+    const { user, logout } = useAuth();
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (open) {
+            const timer = setTimeout(() => { setOpen(false);}, 5000); // 5 sec
+            return () => clearTimeout(timer); 
+        }
+    }, [open]);
+
     return (
         <div className="flex items-center justify-between mb-6 pb-4 border-b border-gray-800">
             <h1 className="text-xl font-semibold">Smart Tourist Safety - Dashboard</h1>
             <div className="flex items-center gap-3">
-                <button className="p-2 rounded-md hover:bg-green-700"><Bell size={18} /></button>
-                <button className="px-4 py-2 bg-gray-800 rounded-md text-sm">Police</button>
+                <button className="p-2 rounded-md cursor-pointer hover:bg-green-700"><Bell size={18} /></button>
+                <div className="relative">
+                    <button onClick={() => setOpen(!open)} className="cursor-pointer px-4 py-2 bg-gray-800 rounded-md text-sm">Police</button>
+                    {open && (
+                        <div className="absolute right-0 mt-2 w-48 bg-gray-900 border border-gray-800 rounded-md shadow-lg">
+                            <div className="px-4 py-2 text-xs text-gray-400 border-b border-gray-800">Logged in as {user.role}</div>
+                            <button onClick={logout} className="cursor-pointer w-full flex items-center gap-2 px-4 py-2 text-sm hover:bg-green-700"><LogOut size={16} />Logout</button>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
